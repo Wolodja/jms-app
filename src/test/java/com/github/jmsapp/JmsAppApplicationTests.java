fd98dd1f7212de9ade54b7a89358662cdb679803
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-05-25 11:39:40 +0200

    Project added

---
+++ src/test/java/com/github/jmsapp/JmsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jmsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
