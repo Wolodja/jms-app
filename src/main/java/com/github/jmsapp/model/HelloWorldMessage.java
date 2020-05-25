f1929d5c52456af2c79a42e1729167e280ade3dd
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-05-25 11:47:44 +0200

    Message object added

---
+++ src/main/java/com/github/jmsapp/model/HelloWorldMessage.java
@@ -0,0 +1,21 @@
+package com.github.jmsapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldMessage implements Serializable {
+
+    static final long serialVersionUID = -2432832496209510648L;
+
+    private UUID id;
+    private String message;
+}
